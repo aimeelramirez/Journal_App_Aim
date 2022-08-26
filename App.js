c3ff5f61c22c0acbc0f541ef7264741b588deda2
aimeelramirez <alramirez1@student.fullsail.edu>
2022-08-25 21:44:23 -0400

    run these cmd on windows

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>template example :)</Text>
+      <Text>The is a another test.</Text>
       <StatusBar style="auto" />
     </View>
   );
